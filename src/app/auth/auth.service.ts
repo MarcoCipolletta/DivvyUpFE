c1c190177742887731b0c71f60cce2482320a768
MarcoCipolletta <marcocipolletta1998@gmail.com>
2025-03-14 15:33:18 +0100

    Fixed login with google

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,18 +3,13 @@ import { LanguageService } from '../services/language.service';
 import { HttpClient } from '@angular/common/http';
 import { iLoginRequest } from './interfaces/i-login-request';
 import { iAuthResponse } from './interfaces/i-auth-response';
-import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   baseUrl = '';
-  constructor(
-    private langSvc: LanguageService,
-    private http: HttpClient,
-    private route: Router
-  ) {
+  constructor(private langSvc: LanguageService, private http: HttpClient) {
     this.langSvc.baseUrl$.subscribe(
       (url) => (this.baseUrl = url + 'auth/login')
     );
@@ -26,6 +21,7 @@ export class AuthService {
 
   loginWithGoogle() {
     let url = this.baseUrl.split('api/')[0] + 'oauth2/authorization/google';
-    this.route.navigateByUrl(url);
+
+    window.location.href = url;
   }
 }
