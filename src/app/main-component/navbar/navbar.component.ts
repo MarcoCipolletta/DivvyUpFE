c16a47dea5004eebc214a6f13a8203efdceb8b1c
MarcoCipolletta <marcocipolletta1998@gmail.com>
2025-03-18 16:18:56 +0100

    wip toggle theme

--- src/app/main-component/navbar/navbar.component.ts
+++ src/app/main-component/navbar/navbar.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { ToggleDarkService } from '../../services/toggle-dark.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(private toggleDarkSvc: ToggleDarkService) {}
+
+  get currentTheme() {
+    return this.toggleDarkSvc.theme;
+  }
+  toggleDark() {
+    this.toggleDarkSvc.toggleTheme();
+  }
+}
