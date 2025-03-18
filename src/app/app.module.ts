2c11a014213678d9f715f7f3baafc354c203d779
MarcoCipolletta <marcocipolletta1998@gmail.com>
2025-03-18 15:48:49 +0100

    add ng-icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,17 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './auth/token.interceptor';
 import { NavbarComponent } from './main-component/navbar/navbar.component';
 import { ButtonsModule } from './shared/buttons/buttons.module';
+import { NgIconsModule } from '@ng-icons/core';
+import { iconoirSettings } from '@ng-icons/iconoir';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule, ButtonsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ButtonsModule,
+    NgIconsModule.withIcons({ iconoirSettings }),
+  ],
   providers: [provideHttpClient(withInterceptors([tokenInterceptor]))],
   bootstrap: [AppComponent],
 })
