923b04ff26c9b6bba2b91820e996be81471213af
MarcoCipolletta <marcocipolletta1998@gmail.com>
2025-03-21 15:06:26 +0100

    set right vh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,20 @@ import { ToggleDarkService } from './services/toggle-dark.service';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  constructor(private toggleDarkSvc: ToggleDarkService) {}
+  constructor(private toggleDarkSvc: ToggleDarkService) {
+    window.addEventListener('resize', this.setViewPort);
+    this.setViewPort();
+  }
 
   toggleDark() {
     this.toggleDarkSvc.toggleTheme();
+  }
+
+  //calcolo l'altezza effettiva della viewport(serve in particolare su mobile)
+  setViewPort() {
+    const vh = window.visualViewport
+      ? window.visualViewport.height * 0.01
+      : window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
   }
 }
